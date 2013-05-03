5db833f713222e92a297de7d89d578a21575935c
Jake Davis <jake.davis5989@gmail.com>
2013-05-03 21:42:56 +0000

    Polyglot user, split up Ruby/JRuby gems, different Android install location

--- attributes/default.rb
+++ attributes/default.rb
@@ -19,6 +19,9 @@
 #
 # Attributes for Polyglot cookbook
 
+# Various defaults
+default[:polyglot][:user] = 'root'
+
 # Maven
 override[:maven][:version] = 3
 override[:maven][:setup_bin] = true
@@ -32,7 +35,7 @@ override[:java][:oracle][:accept_oracle_download_terms] = true
 default[:polyglot][:android][:enable] = false
 default[:polyglot][:android][:sdk_url] = 'http://dl.google.com/android/android-sdk_r21.1-linux.tgz'
 default[:polyglot][:android][:sdk_hash] = '276e3c13a10f37927d4e04d036a94a0cbbf62326981f0ba61a303b76567e3379'
-default[:polyglot][:android][:home] = ::File.join(node[:jenkins][:node][:home],'/tools/android-sdk-linux')
+default[:polyglot][:android][:home] = ::File.join('/opt', 'tools', 'android-sdk-linux')
 default[:polyglot][:android][:filters] = [ 'platform', 'tool', 'platform-tool', 'add-on' ]
 
 # Clojure
@@ -42,11 +45,12 @@ default[:polyglot][:clojure][:lein_checksum] = '00bd30c11a7e9d646ede4487189adde8
 
 # Ruby
 default[:polyglot][:ruby][:enable] = true
-default[:polyglot][:ruby][:gems] = [ 'aws-sdk', 'berkshelf', 'capistrano', 'bundler' ]
-default[:polyglot][:ruby][:gem_cache] = "#{node[:jenkins][:node][:home]}/gem-cache"
+default[:polyglot][:ruby][:ruby_gems] = [ 'aws-sdk', 'berkshelf', 'capistrano', 'bundler' ]
+default[:polyglot][:ruby][:jruby_gems] = [ 'bundler' ]
+default[:polyglot][:ruby][:gem_cache] = ::File.join('/opt', 'gem-cache')
 default[:polyglot][:ruby][:gem_opts] = '--no-ri --no-rdoc'
 default[:polyglot][:ruby][:gem_sources] = []
-override[:rbenv][:rubies] = [ '2.0.0-p0', 'jruby-1.7.3' ]
+override[:rbenv][:rubies] = [ '2.0.0-rc1', 'jruby-1.7.2' ]
 override[:rbenv][:git_ref] = "e3f72ebae20768079ca4b4425a364900f3f16fc6"
 override[:ruby_build][:git_ref] = "724a3f38c4d5e6a89c6772935b40ad76a6b6a889"
 

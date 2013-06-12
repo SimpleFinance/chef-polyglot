62df27ea5268f56dba83d1c705c3dd245262fd2e
Jake Davis <jake.davis5989@gmail.com>
2013-06-12 21:37:58 +0000

    Retroactive version bump for golang default enable

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Simple Finance'
 maintainer_email  'ops@simple.com'
 license           'Apache 2.0'
 description       'Easy defaults for ruby, python, clojure, java, and android'
-version           '1.0.0'
+version           '1.1.0'
 
 depends 'java'
 depends 'maven'

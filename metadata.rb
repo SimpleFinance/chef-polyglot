a027f1b577f1070a34c60e504119e55fbbaa6358
Jake Davis <jake.davis5989@gmail.com>
2013-07-30 04:06:55 +0000

    Switch Golang support to Simple cookbook

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,7 @@ license           'Apache 2.0'
 description       'Easy defaults for ruby, python, clojure, java, and android'
 version           '1.1.0'
 
+depends 'golang'
 depends 'java'
 depends 'maven'
 depends 'python'
